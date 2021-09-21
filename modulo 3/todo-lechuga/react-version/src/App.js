bf0e2ea82386411b92ce25ef6e088aa9fe5c2901
Santiago Bima <santiago.bima@gmail.com>
2021-09-20 23:19:42 -0300

    introduccion de Nav

--- modulo 3/todo-lechuga/react-version/src/App.js
+++ modulo 3/todo-lechuga/react-version/src/App.js
@@ -1,11 +1,19 @@
-import './App.css';
+import Nav from './componentes/layout/Nav'
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 
 function App() {
-  return (
-    <>
-      
-    </>
-  );
+    return (
+        <>
+            <Router>
+                <header>
+                    <Nav></Nav>
+                </header>
+                <Route exact path='/'>
+
+                </Route>
+            </Router>
+        </>
+    );
 }
 
 export default App;
