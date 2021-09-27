0fd936b840ddf0fe9a4aa818181a7110ab109529
Santiago Bima <santiago.bima@gmail.com>
2021-09-27 19:30:43 -0300

    express-version

---
+++ modulo 3/todo-lechuga/express-version/app.js
@@ -0,0 +1,52 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var menuRouter=require('./routes/menu')
+var nosotrosRouter = require('./routes/nosotros');
+var contactanosRouter=require('./routes/contactanos')
+
+var app = express();
+
+// view engine setup
+app.set('appName', 'Todo Lechuga')
+app.set('port', 3001)
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/menu', menuRouter);
+app.use('/contactanos', contactanosRouter);
+app.use('/nosotros', nosotrosRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(app.get('port'), ()=>{
+  console.log(app.get('appName'));
+  console.log('Server on port', app.get('port'));
+})
+
+module.exports = app;
